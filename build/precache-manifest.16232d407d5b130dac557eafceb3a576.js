726c5d3008d9432deee4b708a3c3376c87c181b5
pinkrefugee <pinkrefugee@gmail.com>
2019-02-24 20:34:49 +0300

    feat: add dashboard

---
+++ build/precache-manifest.16232d407d5b130dac557eafceb3a576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4cf97a89d4c21ecfec",
+    "url": "/dashboard/static/js/runtime~main.fa4cf97a.js"
+  },
+  {
+    "revision": "48b11c77b67150906af3",
+    "url": "/dashboard/static/js/main.48b11c77.chunk.js"
+  },
+  {
+    "revision": "5fa5c042afe1238015c3",
+    "url": "/dashboard/static/js/2.5fa5c042.chunk.js"
+  },
+  {
+    "revision": "48b11c77b67150906af3",
+    "url": "/dashboard/static/css/main.ead66111.chunk.css"
+  },
+  {
+    "revision": "4d772db3b045f42c861289d10a337414",
+    "url": "/dashboard/index.html"
+  }
+];
